0441e4ef3e33567a4b6ede6c571141c58d7565ee
lutfihidayat <lutfihidayatr@gmail.com>
2025-02-20 10:53:12 +0700

    initial commit

---
+++ src/main/java/com/lutfi/spchallenge/SpchallengeApplication.java
@@ -0,0 +1,13 @@
+package com.lutfi.spchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpchallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpchallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lutfi/spchallenge/SpchallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lutfi.spchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpchallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
