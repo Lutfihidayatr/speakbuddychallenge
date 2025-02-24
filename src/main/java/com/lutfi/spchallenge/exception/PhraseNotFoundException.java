29bf66af156493b9c0084422c855e83ae12dbaa8
lutfihidayat <lutfihidayatr@gmail.com>
2025-02-24 17:50:39 +0700

    add exception, interface and implementation of phase service

--- src/main/java/com/lutfi/spchallenge/exception/PhraseNotFoundException.java
+++ src/main/java/com/lutfi/spchallenge/exception/PhraseNotFoundException.java
@@ -1,4 +1,23 @@
 package com.lutfi.spchallenge.exception;
 
-public class PhraseNotFoundException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class PhraseNotFoundException extends RuntimeException {
+    public PhraseNotFoundException(String message) {
+        super(message);
+    }
+
+    public PhraseNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public PhraseNotFoundException(Long phraseId) {
+        super(String.format("Phrase not found with id: %d", phraseId));
+    }
+
+    public PhraseNotFoundException(Long userId, Long phraseId) {
+        super(String.format("Phrase not found with id %d for user %d", phraseId, userId));
+    }
 }
--- src/main/java/com/lutfi/spchallenge/service/PhraseService.java
+++ src/main/java/com/lutfi/spchallenge/service/PhraseService.java
@@ -1,4 +1,28 @@
 package com.lutfi.spchallenge.service;
 
+import com.lutfi.spchallenge.entity.Phrase;
+import com.lutfi.spchallenge.entity.User;
+import com.lutfi.spchallenge.repository.PhraseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class PhraseService {
+    private final PhraseRepository phraseRepository;
+
+    @Autowired
+    public PhraseService(PhraseRepository phraseRepository) {
+        this.phraseRepository = phraseRepository;
+    }
+
+    public Phrase save(Phrase phrase) {
+        return phraseRepository.save(phrase);
+    }
+
+    public Optional<Phrase> getPhraseByUserIdAndPhraseId(Long userId, Long phraseId) {
+        return phraseRepository.findByUserIdAndPhraseId(userId, phraseId);
+    }
 }
